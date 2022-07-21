685de6a514169f0cf8bb6a29ee8f96211ec95b5b
Ellie <ellie.fuller@thetrainline.com>
2022-07-21 18:34:45 +0100

    wip

--- TodoApplication/src/Controllers/TodoListController.cs
+++ TodoApplication/src/Controllers/TodoListController.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using TodoApplication.Models;
 
 namespace TodoApplication.Controllers;
 
@@ -6,15 +8,56 @@ namespace TodoApplication.Controllers;
 [Route("[controller]")]
 public class TodoListController : ControllerBase
 {
-    private static readonly string[] Items = new[] { "Chores", "Shopping", "Write a web app API" };
+    private static TodoItem[] Items = new[]
+    {
+        new TodoItem
+        {
+            Name = "Chores"
+        },
+        new TodoItem
+        {
+            Name = "Shopping"
+        },
+        new TodoItem
+        {
+            Name = "Write a web app API"
+        }
+    };
 
     public TodoListController()
     {
     }
 
     [HttpGet(Name = "GetTodoList")]
-    public IEnumerable<string> Get()
+    public IEnumerable<TodoItem> GetItems()
     {
         return Items;
+    }
+
+    [HttpGet("{id:long}", Name = "GetTodoItem")]
+    public ActionResult<TodoItem> GetItem([FromQuery] long id)
+    {
+        try
+        {
+            var item = Items.SingleOrDefault(item => item.Id == id);
+            if (item != null)
+            {
+                return Ok(item);
+            }
+
+            return NotFound();
+        }
+        catch (InvalidOperationException)
+        {
+            return StatusCode(500, "More than one item with the same id");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult AddItem([FromBody] TodoItem todoItem)
+    {
+        var newArray = Items.Append(todoItem).ToArray();
+        Items = newArray;
+        return NoContent();
     }
 }
---
+++ TodoApplication/src/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApplication.Models;
+
+public class TodoItem
+{
+    private static long id;
+    public long Id
+    {
+        get => id;
+        set => id++;
+    }
+
+    [Required]
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItem()
+    {
+        id++;
+    }
+}
