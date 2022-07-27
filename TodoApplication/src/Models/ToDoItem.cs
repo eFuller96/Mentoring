6d14152f38aec4a3372d35dd8605e758874fe304
noeliacar <107847979+noeliacar@users.noreply.github.com>
2022-07-27 14:37:13 +0200

    Adding the model

---
+++ TodoApplication/src/Models/ToDoItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace TodoApplication.Models
+{
+    public class ToDoItem
+    {
+        public int Position { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem (int position, Guid id, string name, bool isCompleted)
+        {
+            Position = position;
+            Id = id;
+            Name = name;
+            IsCompleted = isCompleted;
+        }
+    }
+}
