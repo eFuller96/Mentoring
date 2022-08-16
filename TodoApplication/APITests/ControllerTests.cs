24ee2b83cd88f734be7a4813031f92de2ff6081c
noeliacar <107847979+noeliacar@users.noreply.github.com>
2022-08-16 08:50:42 +0200

    Create unit tests

---
+++ TodoApplication/APITests/ControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApplication.Controllers;
+
+namespace APITests
+{
+    [TestClass]
+    public class ControllerTests
+    {
+
+        [TestMethod]
+        public async void GetMustReturn200httpStatus()
+        {
+            // Arrange
+            TodoListController todoListController = new TodoListController();
+            ActionContext action = new ActionContext();
+
+            // Act
+            var result = todoListController.Get();
+
+
+            //Assert
+            Assert.IsInstanceOfType(result,typeof(OkObjectResult));
+
+        }
+    }
+}
