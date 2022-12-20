877ce66789dbdff6dd677f3b31c963e145b4ad99
noeliacar <107847979+noeliacar@users.noreply.github.com>
2022-12-20 16:07:45 +0100

    Add filepath for csv in Program.cs

--- TodoApplication/src/Program.cs
+++ TodoApplication/src/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddSingleton<ITodoRepository,TodoRepository>();
 // Circular dependency explained in Notes.md
 builder.Services.AddSingleton<IDictionary<Guid, ToDoItem>>(new Dictionary<Guid, ToDoItem>());
 builder.Services.AddSingleton<IDataStorage, FileStorage>();
-//builder.Services.AddSingleton<IDataStorage>(new FileStorage("")); //todo put filepath
+//builder.Services.AddSingleton<IDataStorage>(new FileStorage("ToDoItemsCsv.csv")); //todo put filepath
 
 
 var app = builder.Build();
