ceeba7700be0041ee2fb0250a5ada5373b916025
noeliacar <107847979+noeliacar@users.noreply.github.com>
2022-12-29 12:59:18 +0100

    Exctract csv logic to ICsvFileManager

--- TodoApplication/src/DataStorage/FileStorage.cs
+++ TodoApplication/src/DataStorage/FileStorage.cs
@@ -7,93 +7,44 @@ namespace ToDoApplication.DataStorage;
 public class FileStorage : IDataStorage
 {
     // todo when async app, do with filestorage
-    private static string _fileName;
-    public FileStorage(string fileName)
+    private readonly ICsvFileManager _fileManager;
+
+    public FileStorage(ICsvFileManager fileManager)
     {
-        _fileName = fileName;
+        _fileManager = fileManager;
     }
 
     public async Task<ICollection<ToDoItem>> GetToDoItems()
     {
-        var toDoItems = await GetToDoItemsFromCsv();
+        var toDoItems = await _fileManager.GetToDoItems();
         return toDoItems;
     }
 
     public async Task Add(ToDoItem newItem)
     {
-        var toDoItems = await GetToDoItemsFromCsv();
-        toDoItems.Add(newItem);
-        await WriteCsv(toDoItems);
+        await _fileManager.Add(newItem);
     }
 
     public async Task<ToDoItem> Get(Guid id)
     {
-        if (!ContainsToDoItem(id)) 
+        // todo is it ok to return null? maybe throw
+        var toDoItem = await _fileManager.Get(id);
+        if (toDoItem == null) 
             throw new ItemNotFound($"Item with Id {id} was not found in file");
-        var file = await File.ReadAllLinesAsync(_fileName);
-        var toDoItem = file.Skip(1).SkipLast(1).Select(ParseToDoItem).SingleOrDefault(t => t.Id == id);
         return toDoItem;
     }
 
     public async Task Replace(Guid id, ToDoItem updatedItem)
     {
-        if (!ContainsToDoItem(id))
+        if (_fileManager.Get(id) == null)
             throw new ItemNotFound($"Item could not be replaced as Id {id} was not found in file");
-        var toDoItems = await GetToDoItemsFromCsv();
-        for (var i = 0; i < toDoItems.Count; i++)
-            if (toDoItems[i].Id == id)
-                toDoItems[i] = updatedItem;
-
-        await WriteCsv(toDoItems);
+        await _fileManager.Replace(id, updatedItem);
     }
     // todo delete throw exception?? - maybe not
     public async Task Delete(Guid id)
     {
-        if (!ContainsToDoItem(id))
+        if (await _fileManager.Get(id) is null)
             throw new ItemNotFound($"Item could not be deleted as Id {id} was not found in file");
-        var toDoItems = await GetToDoItemsFromCsv();
-        for (var i = 0; i < toDoItems.Count; i++)
-            if (toDoItems[i].Id == id)
-                toDoItems.Remove(toDoItems[i]);
-        await WriteCsv(toDoItems);
-    }
-
-    private static async Task<List<ToDoItem>> GetToDoItemsFromCsv()
-    {
-        var fileString = await File.ReadAllTextAsync(_fileName);
-        var lines = fileString.Split(Environment.NewLine);
-        var linesWithoutHeader = lines.Skip(1);
-        return (from line in linesWithoutHeader 
-            where line != string.Empty 
-            select line.Split(",") 
-            into values 
-            select new ToDoItem { Id = Guid.Parse(values[0]), Name = values[1], IsCompleted = Convert.ToBoolean(values[2]) }).ToList();
-    }
-
-    private static bool ContainsToDoItem(Guid id)
-    {
-        var toDoItem = File.ReadLines(_fileName).Skip(1).SkipLast(1).Select(ParseToDoItem)
-            .SingleOrDefault(t => t.Id == id);
-        return toDoItem != null;
-    }
-
-    private static ToDoItem ParseToDoItem(string line)
-    {
-        var values = line.Split(',');
-        return new ToDoItem { Id = Guid.Parse(values[0]), Name = values[1], IsCompleted = Convert.ToBoolean(values[2]) };
-    }
-
-    private static async Task WriteCsv(IEnumerable<ToDoItem> toDoItems)
-    {
-        var csv = new StringBuilder();
-        csv.AppendLine("Id,Name,Is Completed");
-        foreach (var newLine in toDoItems.Select(toDoItem => $"{toDoItem.Id},{toDoItem.Name},{toDoItem.IsCompleted}"))
-        {
-            csv.AppendLine(newLine);
-        }
-
-        var lines = csv.ToString().Split(Environment.NewLine);
-
-        await File.WriteAllLinesAsync(_fileName, lines);
+        await _fileManager.Delete(id);
     }
 }
---
+++ TodoApplication/src/DataStorage/ICsvFileManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using ToDoApplication.Models;
+
+namespace ToDoApplication.DataStorage
+{
+    public interface ICsvFileManager
+    {
+        Task <List<ToDoItem>> GetToDoItems();
+        Task Add(ToDoItem newItem);
+        Task<ToDoItem> Get(Guid id);
+        Task Replace(Guid id, ToDoItem updatedItem);
+        Task Delete(Guid id);
+    }
+
+    public class CsvFileManager : ICsvFileManager
+    {
+        private static string _fileName;
+
+        // todo where to put filename
+        public CsvFileManager(string filename)
+        {
+            _fileName = filename;
+        }
+
+        public async Task<List<ToDoItem>> GetToDoItems()
+        {
+            var fileString = await File.ReadAllTextAsync(_fileName);
+            var lines = fileString.Split(Environment.NewLine);
+            var linesWithoutHeader = lines.Skip(1);
+            return (from line in linesWithoutHeader
+                where line != string.Empty
+                select line.Split(",")
+                into values
+                select new ToDoItem { Id = Guid.Parse(values[0]), Name = values[1], IsCompleted = Convert.ToBoolean(values[2]) }).ToList();
+        }
+
+        public async Task Add(ToDoItem newItem)
+        {
+            var toDoItems = await GetToDoItems();
+            toDoItems.Add(newItem);
+            await WriteCsv(toDoItems);
+        }
+
+        public async Task<ToDoItem> Get(Guid id)
+        {
+            // todo make async
+            var toDoItem = File.ReadLines(_fileName).Skip(1).SkipLast(1).Select(ParseToDoItem)
+                .SingleOrDefault(t => t.Id == id);
+            return toDoItem;
+        }
+
+        public async Task Replace(Guid id, ToDoItem updatedItem)
+        {
+            var toDoItems = await GetToDoItems();
+            for (var i = 0; i < toDoItems.Count; i++)
+                if (toDoItems[i].Id == id)
+                    toDoItems[i] = updatedItem;
+            await WriteCsv(toDoItems);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var toDoItems = await GetToDoItems();
+            for (var i = 0; i < toDoItems.Count; i++)
+                if (toDoItems[i].Id == id)
+                    toDoItems.Remove(toDoItems[i]);
+            await WriteCsv(toDoItems);
+        }
+
+        private static ToDoItem ParseToDoItem(string line)
+        {
+            var values = line.Split(',');
+            return new ToDoItem { Id = Guid.Parse(values[0]), Name = values[1], IsCompleted = Convert.ToBoolean(values[2]) };
+        }
+
+        private static async Task WriteCsv(IEnumerable<ToDoItem> toDoItems)
+        {
+            var csv = new StringBuilder();
+            csv.AppendLine("Id,Name,Is Completed");
+            foreach (var newLine in toDoItems.Select(toDoItem => $"{toDoItem.Id},{toDoItem.Name},{toDoItem.IsCompleted}"))
+            {
+                csv.AppendLine(newLine);
+            }
+
+            var lines = csv.ToString().Split(Environment.NewLine);
+
+            await File.WriteAllLinesAsync(_fileName, lines);
+        }
+    }
+}
