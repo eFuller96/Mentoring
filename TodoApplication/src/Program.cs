5b7bc6ae5cde082edbc368f93ad5b452a06b4186
noeliacar <107847979+noeliacar@users.noreply.github.com>
2022-12-29 12:59:18 +0100

    Exctract csv logic to ICsvFileManager

--- TodoApplication/src/Program.cs
+++ TodoApplication/src/Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddSingleton<ITodoRepository,TodoRepository>();
 // Circular dependency explained in Notes.md
 builder.Services.AddSingleton<IDictionary<Guid, ToDoItem>>(new Dictionary<Guid, ToDoItem>());
 builder.Services.AddSingleton<IDataStorage, FileStorage>();
-//builder.Services.AddSingleton<IDataStorage>(new FileStorage("ToDoItemsCsv.csv")); //todo put filepath
+//builder.Services.AddSingleton<IDataStorage>(new FileStorage("ToDoItemsCsv.csv")); 
+builder.Services.AddSingleton<ICsvFileManager>(new CsvFileManager("ToDoItemsCsv.csv"));
 
 
 var app = builder.Build();
