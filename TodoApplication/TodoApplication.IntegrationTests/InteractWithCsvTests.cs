3fd1e83f0da8ec1fad30db057632565c2edb1f60
noeliacar <107847979+noeliacar@users.noreply.github.com>
2023-02-10 14:39:04 +0100

    Add integration tests

--- TodoApplication/TodoApplication.IntegrationTests/InteractWithCsvTests.cs
+++ TodoApplication/TodoApplication.IntegrationTests/InteractWithCsvTests.cs
@@ -19,9 +19,8 @@ namespace TodoApplication.IntegrationTests
         public void SetUp()
         {
             _fileName = "ToDoItemsTest.csv";
-            var filename = "ToDoItemsTest.csv";
             _fileCopy = "ToDoItemsCopy.csv";
-            File.Copy(filename, _fileCopy);
+            File.Copy(_fileName, _fileCopy);
             _csvFileManager = new FileManager(_fileCopy);
         }
 
@@ -35,134 +34,33 @@ namespace TodoApplication.IntegrationTests
         public async Task ReadToDoItemsFromFile()
         {
             var toDoItems = await File.ReadAllLinesAsync(_fileCopy);
-            // todo skip first and compare with result written in csv with file.writealllinesasync
-
 
             var result = await _csvFileManager.ReadToDoItemsFromFile();
-            // file.writealllinesasync from result -- compare both
-            Assert.AreEqual(toDoItems,result);
+            var resultAsString = new List<string>() { "Id,Name,Is Completed" };
+            resultAsString.AddRange(result.Select(toDoItem => $"{toDoItem.Id},{toDoItem.Name},{toDoItem.IsCompleted}"));
+
+            Assert.AreEqual(resultAsString,toDoItems);
         }
 
-        //[Test]
-        //public async Task GetToDoItems_ShouldGetItems_FromCsv()
-        //{
-        //    _fileCopy = "ToDoItemsCopy_GetToDoItems_ShouldGetItems_FromCsv.csv";
-        //    File.Copy(_fileName, _fileCopy);
-        //    _csvFileManager = new FileManager(_fileCopy);
-
-        //    var result = await _csvFileManager.GetToDoItems();
-
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(ToDoItems, result);
-        //}
-
-        //[Test]
-        //public async Task GetToDoItems_ShouldReturnEmptyList_IfCsvIsEmpty()
-        //{
-        //    _fileName = "EmptyCsv.csv";
-        //    _fileCopy = "ToDoItemsCopy_GetToDoItems_ShouldReturnEmptyList_IfCsvIsEmpty.csv";
-        //    File.Copy(_fileName, _fileCopy);
-        //    _csvFileManager = new FileManager(_fileCopy);
-
-        //    var result = await _csvFileManager.GetToDoItems();
-
-        //    Assert.IsNotNull(result);
-        //    Assert.IsEmpty(result);
-        //}
-
-        //[Test]
-        //public async Task Add_ShouldAddItem_InCsv()
-        //{
-        //    _fileCopy = "ToDoItemsCopy_Add_ShouldAddItem_InCsv.csv";
-        //    File.Copy(_fileName, _fileCopy);
-        //    _csvFileManager = new FileManager(_fileCopy);
-        //    var id = Guid.NewGuid();
-        //    var toDoItem = new ToDoItem { Id = id, Name = "name", IsCompleted = false };
-
-        //    await _csvFileManager.Add(toDoItem);
-        //    var result = await _csvFileManager.Get(id);
+        [Test]
+        public async Task WriteToDoItemsInFile()
+        {
+            // todo use a manual list of todoitems instead of reading the same copy
+            var toDoItems = await File.ReadAllLinesAsync(_fileCopy);
+            var toDoItemsWithHeaders = toDoItems.ToList();
+            toDoItemsWithHeaders.Add("");
+            var expected = toDoItemsWithHeaders.ToArray();
+            var toDoItemsWithoutHeaders = toDoItems.Skip(1).ToList();
+            var toDoItemsList = toDoItemsWithoutHeaders.Select(doItem => doItem.Split(",")).Select(items => new ToDoItem() { Id = new Guid(items[0]), Name = items[1], IsCompleted = Convert.ToBoolean(items[2]) }).ToList();
 
 
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(toDoItem, result);
-        //}
+            await _csvFileManager.WriteToDoItemsInFile(toDoItemsList);
 
-        //// TestCaseSource allows objects
-        //[TestCaseSource(nameof(ToDoItems))]
-        //public async Task Get_ShouldGetItemById_WhenItExistsInCsv(ToDoItem toDoItem)
-        //{
-        //    _fileCopy = "ToDoItemsCopy_Get_ShouldGetItemById_WhenItExistsInCsv.csv";
-        //    File.Copy(_fileName, _fileCopy);
-        //    _csvFileManager = new FileManager(_fileCopy);
-
-        //    var result = await _csvFileManager.Get(toDoItem.Id);
-
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(toDoItem, result);
-        //}
-
-        //[Test]
-        //public async Task Get_ShouldReturnNull_WhenItemDoesNotExistInCsv()
-        //{
-        //    _fileCopy = "ToDoItemsCopy_Get_ShouldReturnNull_WhenItemDoesNotExistInCsv.csv";
-        //    File.Copy(_fileName, _fileCopy);
-        //    _csvFileManager = new FileManager(_fileCopy);
-        //    var id = Guid.NewGuid();
-
-        //    var result = await _csvFileManager.Get(id);
-
-        //    Assert.IsNull(result);
-        //}
+            var result = await File.ReadAllLinesAsync(_fileCopy);
 
 
-        //[TestCaseSource(nameof(ToDoItems))]
-        //public async Task Replace_ShouldReplaceItem_InCsv(ToDoItem toDoItem)
-        //{
-        //    _fileCopy = $"ToDoItemsCopy_Replace_ShouldReplaceItem_InCsv_ReplacingItem_{toDoItem.Id}.csv";
-        //    File.Copy(_fileName, _fileCopy);
-        //    _csvFileManager = new FileManager(_fileCopy);
-        //    var id = toDoItem.Id;
-        //    var updatedToDoItem = new ToDoItem { Id = id, Name = "name", IsCompleted = false };
-
-        //    await _csvFileManager.Replace(updatedToDoItem.Id,updatedToDoItem);
-        //    var result = await _csvFileManager.GetToDoItems();
-
-        //    var updatedResult = result.SingleOrDefault(r => r.Id == updatedToDoItem.Id);
-        //    Assert.IsNotNull(result);
-        //    Assert.IsNotNull(updatedResult);
-        //    Assert.AreEqual(updatedToDoItem, updatedResult);
-        //    Assert.AreNotEqual(ToDoItems, result);
-        //}
-
-
-        //private static IEnumerable<ToDoItem> ToDoItems
-        //    => new List<ToDoItem>
-        //    {
-        //        new()
-        //        {
-        //            Id = new Guid("7595022e-f390-4814-91ba-6db90550f46c"),
-        //            Name = "string",
-        //            IsCompleted = true
-        //        },
-        //        new()
-        //        {
-        //            Id = new Guid("dbfddefd-bdeb-4e5e-90bd-bccf865b6068"),
-        //            Name = "update",
-        //            IsCompleted = true
-        //        },
-        //        new()
-        //        {
-        //            Id = new Guid("eb22bf2c-a4aa-420d-a2c5-c3cfcb70b8bd"),
-        //            Name = "string",
-        //            IsCompleted = true
-        //        },
-        //        new()
-        //        {
-        //            Id = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
-        //            Name = "string",
-        //            IsCompleted = true
-        //        }
-        //    };
+            Assert.AreEqual(result, expected);
+        }
 
     }
 }
