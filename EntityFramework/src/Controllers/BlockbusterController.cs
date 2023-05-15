8b01106917f9e810ecc9ce6f3978136b8f806a42
noeliacar <107847979+noeliacar@users.noreply.github.com>
2023-05-15 16:03:37 +0200

    Add implementation for controller

--- EntityFramework/src/Controllers/BlockbusterController.cs
+++ EntityFramework/src/Controllers/BlockbusterController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using System.Net;
+using EntityFramework.Models;
+using EntityFramework.src.DBConfiguration;
+using Flurl;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFramework.Controllers;
 
@@ -7,16 +13,39 @@ namespace EntityFramework.Controllers;
 public class BlockbusterController : ControllerBase
 {
     private readonly ILogger<BlockbusterController> _logger;
+    private readonly MovieContext _movieContext;
 
-    public BlockbusterController(ILogger<BlockbusterController> logger)
+    public BlockbusterController(ILogger<BlockbusterController> logger, MovieContext movieContext)
     {
         _logger = logger;
+        _movieContext = movieContext;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllMovies()
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetMovie([FromRoute] Guid id)
     {
-        // todo
-        return Ok();
+        var movie = _movieContext.Movies.SingleOrDefault(movie => movie.Id == id);
+        return Ok(movie);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddMovie([FromBody] MovieModel movie)
+    {
+        try
+        {
+            _movieContext.Movies.Add(movie);
+            await _movieContext.SaveChangesAsync();
+            // todo how to return id
+            var location = new Url("https://localhost:7028").AppendPathSegment(movie.Id);
+            return Created(location, movie);
+        }
+        // todo catch specific exceptions
+        catch (Exception dbUpdateException)
+        {
+            var objectResult = new ObjectResult("Not able to add movie to db");
+            objectResult.StatusCode = StatusCodes.Status500InternalServerError;
+            return objectResult;
+        }
+
     }
 }
