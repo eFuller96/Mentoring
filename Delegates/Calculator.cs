83ff9142ab0bde59b22f5f0532d06b66ccccadd2
Ellie Fuller <ellie.fuller@thetrainline.com>
2023-06-06 16:03:49 +0100

    Add lesson on delegates

---
+++ Delegates/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace Delegates2._0;
+
+public static class Calculator
+{
+    public static int Calculate(int x, int y, Enum type)
+    {
+        if (type is CalculationType.Add)
+        {
+            return Add(x, y);
+        }
+        else if (type is CalculationType.Minus)
+        {
+            return Minus(x, y);
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    private static int Add(int x, int y)
+        => x + y;
+
+    private static int Minus(int x, int y)
+        => x - y;
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task:
+// 1. Add multiplier and divider method to the Calculator
+// 2. Refactor Calculator to use delegates
+
+
+using Delegates2._0;
+
+Console.WriteLine("enter calculation to perform (options: [Add|Minus]): ");
+var userInput = Console.ReadLine();
+Enum.TryParse(userInput, out CalculationType calculationType);
+
+Console.WriteLine("enter first value: ");
+var variableOne = Console.ReadLine();
+Console.WriteLine("enter second value: ");
+var variableTwo = Console.ReadLine();
+
+var result = Calculator.Calculate(int.Parse(variableOne), int.Parse(variableTwo), calculationType);
+
+Console.WriteLine();
+Console.WriteLine(result);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
